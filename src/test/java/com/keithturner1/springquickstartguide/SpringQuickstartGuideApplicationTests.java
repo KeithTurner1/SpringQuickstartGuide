0e04328c770175fbe479b3070bb7489a13b27555
Keith <turnerkj_git@protonmail.com>
2021-05-29 11:46:17 -0400

    Initial Commit

---
+++ src/test/java/com/keithturner1/springquickstartguide/SpringQuickstartGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keithturner1.springquickstartguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuickstartGuideApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
